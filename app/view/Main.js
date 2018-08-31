024b22fff0e796d2cd7de325b632de0ca680cef4
Rokyed <rokyed@gmail.com>
2018-08-31 16:05:17 -0400

    bla

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,11 +1,64 @@
+import './MyOtherClass.js'
+import './MyOtherOtherClass.js'
+
 Ext.define('Test.view.Main', {
 	xtype: 'view-main',
-	extend: 'Ext.Component',
+	extend: 'Test.view.View',
 
 	config: {
 		tpl: `
-			<div> works!!</div>
+			<div> wkjhkjhorks!!</div>
+			<div class="where-we-render"></div>
 		`,
-		data: {}
+
+		data: {},
+
+		myOtherClass: null,
+
+		myOtherOtherClass: null,
+
+	},
+
+	constructor(config) {
+		debugger
+		this.callParent()
+
+		this.on({
+			render: this.whenIsRendered,
+			scope: this
+		})
+
+		console.log('this works')
+	},
+
+	whenIsRendered() {
+		this.setMyOtherClass({})
+		this.setMyOtherOtherClass({})
+	},
+
+	applyMyOtherClass(config, instance) {
+		if (instance)
+			instance.destroy()
+
+		if (!config)
+			return config
+
+		return Ext.factory(Ext.apply({
+			xtype: 'view-myotherclass',
+			renderTo: this.el.dom.querySelector('.where-we-render')
+		}, config))
+	},
+
+	applyMyOtherOtherClass(config, instance) {
+		if (instance)
+			instance.destroy()
+
+		if (!config)
+			return config
+
+		return Ext.factory(Ext.apply({
+			xtype: 'view-myotherotherclass',
+			renderTo: this.el.dom.querySelector('.where-we-render')
+		}, config))
 	}
 })
